5cb322332bb4f64a6f39b91daba8d1244cc9a4d3
Marco Caglia <cagliamarco@gmail.com>
2019-06-06 21:04:41 +0900

    Starting to implement Deep Learning Model
    Due to technical difficulties, pytorch was replaced with keras.
    Models are up and working. The next step will be some fine tuning of the
    GBR and the implementation of the XGBR. After that, Submissions can be
    made.

--- kaggle_scorer.py
+++ kaggle_scorer.py
@@ -4,3 +4,7 @@ def rmsle(labels,predictions):
     root_mean_squared_error = np.mean((np.log(labels)-np.log(predictions))**2)**0.5
 
     return root_mean_squared_error
+
+def rmsle_validation(ytest, ypred):
+	assert len(ytest) == len(ypred)
+	return np.sqrt(np.mean((np.log1p(ypred) - np.log1p(ytest))**2))
