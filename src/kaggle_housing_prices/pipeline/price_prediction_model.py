db1d6013632b088cac17a8f1227b22e40827081a
marco <cagliamarco@gmail.com>
2022-07-10 12:02:04 +0200

    Feature (price-prediction-model): Add skopt based bayesCV pipeline
    Added a standard sklearn pipeline, which uses skopt's implementation of
    BayesCV to tune hyperparameters during fit

--- src/kaggle_housing_prices/pipeline/price_prediction_model.py
+++ src/kaggle_housing_prices/pipeline/price_prediction_model.py
@@ -3,12 +3,19 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, Union
 
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
 from kaggle_housing_prices.process import feature_engineering, preprocessing, regression
+from sklearn.feature_selection import SelectPercentile, mutual_info_regression
+from sklearn.metrics import mean_squared_log_error
+from sklearn.model_selection import cross_validate
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
+from skopt import BayesSearchCV
+from skopt.space import Categorical, Integer, Real
 
 
 class BasePriceModel(ABC):
@@ -137,3 +144,93 @@ class PriceModel(BasePriceModel):
         regression_report = self.regressor.get_report(self.predictions, y)
 
         return {**fe_report, **regression_report}
+
+
+class PricePredictionPipeline(PriceModel):
+
+    # Define scope of hyperparameters
+    SEARCH_SPACE = {
+        "scaler": Categorical(
+            [StandardScaler(), MinMaxScaler(), RobustScaler(), "passthrough"]
+        ),
+        "outlier_marker__n_estimators": Integer(50, 500, "uniform"),
+        "feature_selector__percentile": Integer(50, 100, "uniform"),
+        "regressor__grad_boost__learning_rate": Real(0.01, 1, prior="log-uniform"),
+        "regressor__grad_boost__n_estimators": Integer(50, 500, prior="log-uniform"),
+        "regressor__grad_boost__subsample": Real(0.1, 1.0, prior="uniform"),
+        "regressor__grad_boost__criterion": Categorical(["friedman_mse", "mse"]),
+        "regressor__grad_boost__min_samples_split": Integer(
+            2, 100, prior="log-uniform"
+        ),
+        "regressor__grad_boost__min_samples_leaf": Integer(1, 100, prior="log-uniform"),
+        "regressor__grad_boost__max_depth": Integer(1, 10, prior="log-uniform"),
+        # "regressor__grad_boost__min_impurity_decrease": Real(
+        #     0.0, 10.0, prior="log-uniform"
+        # ),
+        # "regressor__svr__C": Real(1.0, 1e10, prior="uniform"),
+        "regressor__ridge__alpha": Real(1e-15, 1.0, "log-uniform"),
+    }
+
+    def __init__(
+        self,
+        preprocessor: preprocessing.BasePreprocessor,
+        feature_engineer: feature_engineering.BaseFeatureEngineer,
+        regressor: regression.BaseRegressor,
+        **kwargs,
+    ) -> None:
+        """Construct a SKlearn Pipeline from the passed components."""
+        pipeline = Pipeline(
+            [
+                ("preprocessing", preprocessor),
+                ("encoder", feature_engineer),
+                ("scaler", "passthrough"),
+                ("outlier_marker", feature_engineering.OutlierMarker()),
+                (
+                    "feature_selector",
+                    SelectPercentile(mutual_info_regression, percentile=100),
+                ),
+                ("regressor", regressor),
+            ]
+        )
+        self.bayes_pipe = BayesSearchCV(
+            estimator=pipeline,
+            search_spaces=self.SEARCH_SPACE,
+            refit=True,
+            return_train_score=True,
+            scoring=self.rmsle_score,
+        )
+
+        for attr, value in kwargs.items():
+            if attr in vars(self.bayes_pipe):
+                setattr(self.bayes_pipe, attr, value)
+
+    def fit(
+        self, X: pd.DataFrame, y: npt.NDArray[np.float32], **kwargs
+    ) -> BasePriceModel:
+        """Fit pipeline with hyperparameter tuning."""
+        self.bayes_pipe.fit(X, y)
+
+        return self
+
+    def predict(
+        self, X: pd.DataFrame, y: Union[npt.NDArray[np.float32], None] = None, **kwargs
+    ) -> npt.NDArray[np.float32]:
+        return self.bayes_pipe.predict(X)
+
+    def get_report(
+        self, X: pd.DataFrame, y: Union[npt.NDArray[np.float32], None] = None, **kwargs
+    ) -> Dict[str, Any]:
+        return cross_validate(
+            self.bayes_pipe,
+            X,
+            y,
+            scoring=self.rmsle_score,
+            return_train_score=True,
+            **kwargs,
+        )
+
+    @staticmethod
+    def rmsle_score(model, X, y):
+        y_hat = model.predict(X)
+        y_hat[np.where(y_hat <= 0)] = 0
+        return (mean_squared_log_error(y, y_hat) ** 0.5) * -1
